976ac17cac134f19c7073c72faabed4bc6722e49
viol33 <alininavcisi@gmail.com>
2020-10-18 17:00:48 +0300

    Rotator added

---
+++ HCHelpers/World/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ali.Helper.World
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _rotateVelocity;
+        [SerializeField] private Space _rotateSpace;
+
+        void Update()
+        {
+            transform.Rotate(_rotateVelocity * Time.deltaTime, _rotateSpace);
+        }
+    }
+}
