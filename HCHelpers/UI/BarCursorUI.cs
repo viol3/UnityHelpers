c990657a7ee5fbde042ea03d238cc9d7866e6847
aliavci <aliavci1380@yahoo.com>
2020-10-29 10:48:20 +0300

    bar UI simplified and BarCursorUI added

---
+++ HCHelpers/UI/BarCursorUI.cs
@@ -0,0 +1,54 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ali.Helper.UI
+{
+    public class BarCursorUI : MonoBehaviour
+    {
+        [SerializeField] private bool _horizontal = true;
+        [SerializeField] private float _min;
+        [SerializeField] private float _max;
+
+        private RectTransform _rectTransform;
+        private Tweener _moveTween;
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+        public void SetRatio(float newRatio, float speed)
+        {
+            float newAxesValue = _min + (_max - _min) * newRatio;
+            if (_moveTween != null)
+            {
+                _moveTween.Kill();
+            }
+            if (_horizontal)
+            {
+                _moveTween = _rectTransform.DOAnchorPosX(newAxesValue, speed).SetSpeedBased();
+            }
+            else
+            {
+                _moveTween = _rectTransform.DOAnchorPosY(newAxesValue, speed).SetSpeedBased();
+            }
+        }
+
+        public void SetRatio(float newRatio)
+        {
+            float newAxesValue = _min + (_max - _min) * newRatio;
+            if (_moveTween != null)
+            {
+                _moveTween.Kill();
+            }
+            if (_horizontal)
+            {
+                _rectTransform.anchoredPosition = new Vector2(newAxesValue, _rectTransform.anchoredPosition.y);
+            }
+            else
+            {
+                _rectTransform.anchoredPosition = new Vector2(_rectTransform.anchoredPosition.x, newAxesValue);
+            }
+        }
+    }
+}
