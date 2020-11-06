efe2ec35bf430c147e5f428cab2703fefa51c2b6
viol33 <alininavcisi@gmail.com>
2020-11-07 01:16:27 +0300

    dialogue system added

---
+++ HCHelpers/UI/Dialogue/DialogueChoice.cs
@@ -0,0 +1,58 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ali.Helper.UI.Dialogue
+{
+    public class DialogueChoice : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _choicePanel;
+        [SerializeField] private Text _choiceText;
+        [SerializeField] private int _choiceIndex = 0;
+
+        public event System.Action<int> OnClick;
+
+        private bool _alreadyChoiced = false;
+
+        public void OnChoiceClick()
+        {
+            if (_alreadyChoiced)
+            {
+                return;
+            }
+            _alreadyChoiced = true;
+            Bounce();
+            OnClick?.Invoke(_choiceIndex);
+        }
+
+        public void SetText(string text)
+        {
+            _choiceText.text = text;
+        }
+
+        public void Show(float duration)
+        {
+            _choicePanel.gameObject.SetActive(true);
+            _choicePanel.localScale *= 0.5f;
+            _choicePanel.DOScale(transform.localScale * 2f, duration);
+        }
+
+        public void Hide()
+        {
+            _choicePanel.gameObject.SetActive(false);
+        }
+
+        public void Reset()
+        {
+            _alreadyChoiced = false;
+        }
+
+        void Bounce()
+        {
+            _choicePanel.DOPunchScale(Vector3.one * 0.1f, 0.3f, 6);
+        }
+
+    }
+}
---
+++ HCHelpers/UI/Dialogue/DialogueManager.cs
@@ -0,0 +1,65 @@
+﻿using Ali.Helper;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ali.Helper.UI.Dialogue
+{
+    public class DialogueManager : LocalSingleton<DialogueManager>
+    {
+        [SerializeField] private DialogueQuestion _questionDialogue;
+        [SerializeField] private DialogueRaw _rawDialogue;
+
+        private DialogueBase _currentDialogue;
+        private bool _activeForInteraction = false;
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        public IEnumerator WaitForInteract()
+        {
+            yield return _currentDialogue.WaitForInteract();
+        }
+
+        public IEnumerator ShowQuestion(string question, int correctChoiceIndex, params string[] choices)
+        {
+            _questionDialogue.gameObject.SetActive(true);
+            _activeForInteraction = false;
+            _questionDialogue.SetContentText(question);
+            _questionDialogue.SetCorrectAnswer(correctChoiceIndex);
+            _questionDialogue.SetChoicesTexts(choices);
+            _currentDialogue = _questionDialogue;
+            yield return _currentDialogue.Show();
+            _activeForInteraction = true;
+        }
+
+        public IEnumerator ShowRaw(string content)
+        {
+            _rawDialogue.gameObject.SetActive(true);
+            _activeForInteraction = false;
+            _rawDialogue.SetContentText(content);
+            _currentDialogue = _rawDialogue;
+            yield return _currentDialogue.Show();
+            _activeForInteraction = true;
+        }
+
+        public IEnumerator HideCurrentDialogue()
+        {
+            yield return _currentDialogue.Hide();
+            _currentDialogue.Reset();
+            DisableAllPanels();
+        }
+
+        public bool IsQuestionAnswerCorrect()
+        {
+            return _questionDialogue.IsAnswerCorrect();
+        }
+
+        void DisableAllPanels()
+        {
+            _questionDialogue.gameObject.SetActive(false);
+            _rawDialogue.gameObject.SetActive(false);
+        }
+    }
+}
