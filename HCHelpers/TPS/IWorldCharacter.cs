9ec47f0337d45b5ab76335b3129ba6ff77d1b733
aliavci <aliavci1380@yahoo.com>
2020-11-16 17:38:57 +0300

    tps character removed and world character added

---
+++ HCHelpers/TPS/IWorldCharacter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWorldCharacter
+{
+    void ProcessVelocity(Vector3 velocity);
+    void PlayAnimationState(string stateName);
+
+}
---
+++ HCHelpers/TPS/IWorldCharacterAnimator.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWorldCharacterAnimator
+{
+   void PlayAnimationState(string stateName);
+}
