39adbb0273e43e1bf76516e2a2bb371d5d868413
viol33 <alininavcisi@gmail.com>
2021-01-12 23:15:11 +0300

    blendshape face animator, position clamper, simplemove

---
+++ HCHelpers/World/BSFaceAnimator.cs
@@ -0,0 +1,36 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ali.Helper.World
+{
+    public class BSFaceAnimator : MonoBehaviour
+    {
+        private SkinnedMeshRenderer _smr;
+
+        private void Start()
+        {
+            _smr = GetComponent<SkinnedMeshRenderer>();
+        }
+
+        public void MoveBSValueTo(string bsName, float value, float duration)
+        {
+            DOTween.To(() => _smr.GetBlendShapeWeight(GetBSIndexByName(bsName)), x => _smr.SetBlendShapeWeight(GetBSIndexByName(bsName), x), value, duration);
+        }
+
+        int GetBSIndexByName(string bsName)
+        {
+            _smr = GetComponent<SkinnedMeshRenderer>();
+            Mesh m = _smr.sharedMesh;
+            for (int i = 0; i < m.blendShapeCount; i++)
+            {
+                if (m.GetBlendShapeName(i).Equals(bsName))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
