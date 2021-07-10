7ef251bde1b2be36a86c80d1af1c18d622235847
viol33 <alininavcisi@gmail.com>
2021-07-10 15:00:35 +0300

    player swerve added

---
+++ HCHelpers/Helper Scripts/Runner/PlayerSwerve.cs
@@ -0,0 +1,32 @@
+﻿using Lean.Touch;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ali.Helper.Runner
+{
+    public class PlayerSwerve : MonoBehaviour
+    {
+        [SerializeField] private float _speedMultiplier = 1f;
+
+        public event System.Action<float> OnSwerve;
+        void Start()
+        {
+            LeanTouch.OnFingerUpdate += LeanTouch_OnFingerUpdate;
+        }
+
+        private void LeanTouch_OnFingerUpdate(LeanFinger finger)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                OnSwerve?.Invoke(finger.ScaledDelta.x * _speedMultiplier);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            LeanTouch.OnFingerUpdate -= LeanTouch_OnFingerUpdate;
+        }
+
+    }
+}
